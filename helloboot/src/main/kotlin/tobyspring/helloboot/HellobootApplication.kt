dfc93bf2b11477364577d65fb62045adb24e3b45
solly29 <cksdud1311@naver.com>
2023-02-20 00:37:04 +0900

    Container less 2
    빈 컨테이너 생성

--- helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
@@ -1,5 +1,10 @@
 package tobyspring.helloboot
 
+import org.apache.catalina.startup.Tomcat
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.server.WebServer
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+
 //import org.springframework.boot.autoconfigure.SpringBootApplication
 //import org.springframework.boot.runApplication
 
@@ -8,5 +13,11 @@ class HellobootApplication
 
 fun main(args: Array<String>) {
 //    runApplication<HellobootApplication>(*args)
-    println("Hello Containerless Standalone Application")
+    // Spring boot에서 재공
+    val server: ServletWebServerFactory = TomcatServletWebServerFactory()
+
+    // spring boot에서 톰캣 외의 다른 Servlet Container도 지원함
+    val webServer: WebServer = server.getWebServer()
+
+    webServer.start()
 }
