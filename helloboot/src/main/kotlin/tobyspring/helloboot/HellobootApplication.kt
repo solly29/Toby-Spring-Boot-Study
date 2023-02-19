f7f19b483f8e3cc4bc4fae86bbfef32e59d7a8c9
solly29 <cksdud1311@naver.com>
2023-02-20 00:54:48 +0900

    Container less 2
    서블릿 컨테이너 세팅

--- helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
@@ -3,7 +3,14 @@ package tobyspring.helloboot
 import org.apache.catalina.startup.Tomcat
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.server.WebServer
+import org.springframework.boot.web.servlet.ServletContextInitializer
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 //import org.springframework.boot.autoconfigure.SpringBootApplication
 //import org.springframework.boot.runApplication
@@ -17,7 +24,21 @@ fun main(args: Array<String>) {
     val server: ServletWebServerFactory = TomcatServletWebServerFactory()
 
     // spring boot에서 톰캣 외의 다른 Servlet Container도 지원함
-    val webServer: WebServer = server.getWebServer()
+    val webServer: WebServer = server.getWebServer(ServletContextInitializer {
+        it.addServlet("hello", object : HttpServlet() {
+            override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
+
+                val name = req?.getParameter("name")
+
+                // 응답
+                resp?.apply {
+                    status = HttpStatus.OK.value()
+                    setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+                    writer.println("Hello $name")
+                }
+            }
+        }).addMapping("/hello")
+    })
 
     webServer.start()
 }
