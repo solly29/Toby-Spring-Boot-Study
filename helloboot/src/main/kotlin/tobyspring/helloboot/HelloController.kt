bc1d44d5d3d4feaa0598617fc40a3356ed30d60a
solly29 <cksdud1311@naver.com>
2023-02-20 22:26:01 +0900

    DispatcherServlet 적용 2
    매핑 정보 작성 어노테이션 이용

--- helloboot/src/main/kotlin/tobyspring/helloboot/HelloController.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HelloController.kt
@@ -1,15 +1,19 @@
 package tobyspring.helloboot
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 //@RestController
+@RequestMapping("/hello")
 class HelloController(
     private val helloService: HelloService
 ) {
 
-//    @GetMapping("/hello")
+    @GetMapping
+    @ResponseBody
     fun hello(name: String?): String {
         return name?.let { helloService.sayHello(it) } ?: throw NullPointerException()
     }
