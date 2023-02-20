cccb8382567bc9bd25022e20db48c12a778726a3
solly29 <cksdud1311@naver.com>
2023-02-20 22:13:04 +0900

    DispatcherServlet 적용

--- helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.web.context.support.GenericWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -24,7 +26,7 @@ class HellobootApplication
 fun main(args: Array<String>) {
 //    runApplication<HellobootApplication>(*args)
 
-    val applicationContext = GenericApplicationContext()
+    val applicationContext = GenericWebApplicationContext()
     // 빈 등롤
     applicationContext.registerBean(HelloController::class.java, BeanDefinitionCustomizer {  })
     applicationContext.registerBean(SimpleHelloService::class.java, BeanDefinitionCustomizer {  })
@@ -36,29 +38,9 @@ fun main(args: Array<String>) {
 
     // spring boot에서 톰캣 외의 다른 Servlet Container도 지원함
     val webServer: WebServer = server.getWebServer(ServletContextInitializer {
-
-        it.addServlet("frontcontroller", object : HttpServlet() {
-            override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
-
-                // 인증, 보안, 다국어 등등의 공동 기능 처리
-                if(req?.requestURI == "/hello" && req.method == HttpMethod.GET.name) {
-                    // 바인딩
-                    val name = req.getParameter("name")
-
-                    val helloController = applicationContext.getBean(HelloController::class.java)
-                    val result = helloController.hello(name)
-
-                    // 응답
-                    resp?.apply {
-                        contentType = MediaType.TEXT_PLAIN_VALUE
-                        writer.println(result)
-                    }
-                } else {
-                    resp?.status = HttpStatus.NOT_FOUND.value()
-                }
-
-            }
-        }).addMapping("/*")
+        it.addServlet("dispatcherServlet",
+            DispatcherServlet(applicationContext)
+        ).addMapping("/*")
     })
 
     webServer.start()
