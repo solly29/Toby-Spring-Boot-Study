6e1e3986bf1c3a7f05a6361483476ea70a300405
solly29 <cksdud1311@naver.com>
2023-02-20 20:45:12 +0900

    스프링 컨테이너 사용

--- helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
@@ -1,10 +1,12 @@
 package tobyspring.helloboot
 
 import org.apache.catalina.startup.Tomcat
+import org.springframework.beans.factory.config.BeanDefinitionCustomizer
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.server.WebServer
 import org.springframework.boot.web.servlet.ServletContextInitializer
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.context.support.GenericApplicationContext
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
@@ -21,31 +23,34 @@ class HellobootApplication
 
 fun main(args: Array<String>) {
 //    runApplication<HellobootApplication>(*args)
+
+    val applicationContext = GenericApplicationContext()
+    applicationContext.registerBean(HelloController::class.java, BeanDefinitionCustomizer {  })
+    // 컨테이너 초기화
+    applicationContext.refresh()
+
     // Spring boot에서 재공
     val server: ServletWebServerFactory = TomcatServletWebServerFactory()
 
     // spring boot에서 톰캣 외의 다른 Servlet Container도 지원함
     val webServer: WebServer = server.getWebServer(ServletContextInitializer {
 
-        val helloController = HelloController()
-
         it.addServlet("frontcontroller", object : HttpServlet() {
             override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
 
                 // 인증, 보안, 다국어 등등의 공동 기능 처리
                 if(req?.requestURI == "/hello" && req.method == HttpMethod.GET.name) {
+                    // 바인딩
                     val name = req.getParameter("name")
 
+                    val helloController = applicationContext.getBean(HelloController::class.java)
                     val result = helloController.hello(name)
 
                     // 응답
                     resp?.apply {
-                        status = HttpStatus.OK.value()
-                        setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+                        contentType = MediaType.TEXT_PLAIN_VALUE
                         writer.println(result)
                     }
-                } else if(req?.requestURI == "/user") {
-
                 } else {
                     resp?.status = HttpStatus.NOT_FOUND.value()
                 }
