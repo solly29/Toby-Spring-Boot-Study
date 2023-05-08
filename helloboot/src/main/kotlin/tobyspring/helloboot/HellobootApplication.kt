de41ca70de2f09899c4dec4065dcd5b18730a89d
solly29 <cksdud1311@naver.com>
2023-05-08 22:49:08 +0900

    SpringBootApplication 수정

--- helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
+++ helloboot/src/main/kotlin/tobyspring/helloboot/HellobootApplication.kt
@@ -1,30 +1,12 @@
 package tobyspring.helloboot
 
-import org.apache.catalina.startup.Tomcat
-import org.springframework.beans.factory.config.BeanDefinitionCustomizer
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.server.WebServer
-import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.support.GenericApplicationContext
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
-import org.springframework.web.context.support.GenericWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
-import org.springframework.core.io.ResourceLoader
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import kotlin.reflect.KClass
 
 //import org.springframework.boot.autoconfigure.SpringBootApplication
 //import org.springframework.boot.runApplication
@@ -41,6 +23,5 @@ class HellobootApplication {
 }
 
 fun main(args: Array<String>) {
-//    runApplication<HellobootApplication>(*args)
-    run<HellobootApplication>(*args)
+    runApplication<HellobootApplication>(*args)
 }
