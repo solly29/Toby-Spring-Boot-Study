ea63e83eadf7db1b666e39c671597ba4c6d280cd
solly29 <cksdud1311@naver.com>
2023-05-25 21:17:29 +0900

    @Conditional과 Condition

--- helloboot/src/main/kotlin/tobyspring/config/autoconfig/JettyWebServerConfig.kt
+++ helloboot/src/main/kotlin/tobyspring/config/autoconfig/JettyWebServerConfig.kt
@@ -3,12 +3,18 @@ package tobyspring.config.autoconfig
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.*
+import org.springframework.core.type.AnnotatedTypeMetadata
 import tobyspring.config.MyAutoConfig
 
 @MyAutoConfig
+@Conditional(JettyWebServerConfig.JettyCondition::class)
 class JettyWebServerConfig {
+    class JettyCondition : Condition {
+        override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+            return false
+        }
+    }
 
     @Bean("jettyWebServerFactory")
     fun servletWebServerFactory(): ServletWebServerFactory = JettyServletWebServerFactory()
--- helloboot/src/main/kotlin/tobyspring/config/autoconfig/TomcatWebServerConfig.kt
+++ helloboot/src/main/kotlin/tobyspring/config/autoconfig/TomcatWebServerConfig.kt
@@ -2,12 +2,18 @@ package tobyspring.config.autoconfig
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.*
+import org.springframework.core.type.AnnotatedTypeMetadata
 import tobyspring.config.MyAutoConfig
 
 @MyAutoConfig
+@Conditional(TomcatWebServerConfig.TomcatCondition::class)
 class TomcatWebServerConfig {
+    class TomcatCondition : Condition {
+        override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+            return true
+        }
+    }
 
     @Bean("tomcatWebServerFactory")
     fun servletWebServerFactory(): ServletWebServerFactory = TomcatServletWebServerFactory()
